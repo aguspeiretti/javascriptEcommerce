ee35aea1295194383d86216d6430aa02cea1423a
aguspeiretti <aguspeiretti@gmail.com>
2022-12-02 16:23:41 -0300

    second commit

--- productos.js
+++ productos.js
@@ -1,13 +1,20 @@
 
 const contenedorProductos = document.getElementById("productos")
+const btnCarrito = document.getElementById("btnCarrito")
+const btnCerrarCarrito = document.getElementById("btnCerrarCarrito")
+const cartContainer = document.getElementById("cartContainer")
+const numerito1 = JSON.parse(localStorage.getItem("numerito"))
+const numCarrito = document.getElementById("numerito")
+const contenido = document.getElementById("contenido")
+console.log(contenido)
+let carrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
 
-
+  numCarrito.innerText = numerito1
 
   const articulos =  fetch('https://fakestoreapi.com/products/')
             .then(res=>res.json())
             .then(data=>{
                 let hombre = data.filter(elem => elem.category == "men's clothing")
-                console.log(hombre)
                 hombre.forEach(prducto =>  {
                     const { title, category, description, id , image ,price ,rating } = prducto;
                     const card = document.createElement("div");
@@ -26,8 +33,37 @@ const contenedorProductos = document.getElementById("productos")
                     contenedorProductos.append(card)
                 
             })  
-   
+        })
+
+
+
+       
+
+        const abrirCarrito = ()=>{ cartContainer.classList.toggle("abierto")}
+        const cerrarCarrito = ()=>{ cartContainer.classList.toggle("abierto")}
+        btnCarrito.addEventListener("click" , abrirCarrito)
+        btnCerrarCarrito.addEventListener("click" , cerrarCarrito)
         
 
+       
+        contenido.innerHTML = ""
 
-        })
+        const mostrarCarrito = ()=>{
+          contenido.innerHTML = ""
+          carrito.forEach((prducto)=>{
+            const carring = document.createElement("div");
+            carring.className = "carring";
+            carring.innerHTML = `
+            <img src=${prducto.image} alt="" />
+            <div class="carring-info">
+            <h2>${prducto.title}</h2>
+            </div>
+            <div class="carring-price" >
+            <button data-id="${prducto.title}" class="vaciarCarrito">X</button>
+            <p> $${prducto.price}</p>
+            </div>
+            `;
+             contenido.append(carring);
+           });
+          }
+          mostrarCarrito()
--- productosShop.js
+++ productosShop.js
@@ -2,23 +2,47 @@ const loader = document.querySelector(".loader")
 const contenedorProductosShop = document.getElementById("productosShop")
 const selectores = document.querySelectorAll("#selectores")
 const botonesProductos = document.querySelectorAll(".btnCard")
+const numerito = document.querySelector("#numerito")
+const numeritoJson = JSON.parse(localStorage.getItem("numerito"))
+const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"))
+numerito.innerText = numeritoJson
+const contenido = document.getElementById("contenido")
 
+let carrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
+
+console.log(carrito)
 
 const fetchProductos = fetch('https://fakestoreapi.com/products/')
           .then(res=>res.json())
           .then(data=>{
             let productos = data
-            console.log(productos)
+                      
 
-            let carrito = []
-
-
+            const actualizarNumerito =()=>{
+              let nuevoNumerito = carrito.reduce((acc , producto)=> acc + producto.cantidad , numeritoJson)
+              numerito.innerText = nuevoNumerito
+              localStorage.setItem("numerito" , JSON.stringify(nuevoNumerito))
+            }
+    
             const agregarProductoCarrito = (e)=>{
             const idBOton = e.currentTarget.id;
             const productoAgregado = productos.find(producto => producto.title === idBOton )
-            carrito.push(productoAgregado)
-            console.log(carrito)
+
+            if (carrito.some(producto => producto.title === idBOton)){
+              const index = carrito.findIndex(producto => producto.title === idBOton)
+              carrito[index].cantidad++
+            }else{
+              productoAgregado.cantidad = 1
+              carrito.push(productoAgregado)
             }
+
+            actualizarNumerito()
+           
+            localStorage.setItem("productos-en-carrito", JSON.stringify(carrito))
+            console.log(carrito)
+            
+            }
+          
             
             const actualizarBotonesShop = () =>{
             
@@ -87,9 +111,31 @@ selectores.forEach(elem =>{
 
 
 
-        
+const abrirCarrito = ()=>{ cartContainer.classList.toggle("abierto")}
+const cerrarCarrito = ()=>{ cartContainer.classList.toggle("abierto")}
+btnCarrito.addEventListener("click" , abrirCarrito)
+btnCerrarCarrito.addEventListener("click" , cerrarCarrito)     
 
 
-   
-       
-       
+
+contenido.innerHTML = ""
+
+const mostrarCarrito = ()=>{
+  contenido.innerHTML = ""
+  carrito.forEach((prducto)=>{
+    const carring = document.createElement("div");
+    carring.className = "carring";
+    carring.innerHTML = `
+    <img src=${prducto.image} alt="" />
+    <div class="carring-info">
+    <h2>${prducto.title}</h2>
+    </div>
+    <div class="carring-price" >
+    <button data-id="${prducto.title}" class="vaciarCarrito">X</button>
+    <p> $${prducto.price}</p>
+    </div>
+    `;
+     contenido.append(carring);
+   });
+  }
+  mostrarCarrito()
